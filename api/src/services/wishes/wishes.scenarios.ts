b1c7ae8caff39f4eaec7b31ac92b5f922613986b
Billy Duraney <william.duraney@gmail.com>
2022-12-06 20:58:47 +0200

    stub wish

--- api/src/services/wishes/wishes.scenarios.ts
+++ api/src/services/wishes/wishes.scenarios.ts
@@ -2,7 +2,14 @@ import type { Prisma, Wish } from '@prisma/client'
 import type { ScenarioData } from '@redwoodjs/testing/api'
 
 export const standard = defineScenario<Prisma.WishCreateArgs>({
-  wish: { one: { data: {} }, two: { data: {} } },
+  wish: { one: { data: {
+    title: "Test 1",
+    description: "Test wish",
+    url: "www.example.com/test1",
+  } }, two: { data: {
+    title: "Test 1",
+    description: "Test wish",
+  } } },
 })
 
 export type StandardScenario = ScenarioData<Wish, 'wish'>
