6f0d5131be81b6dca747c8ebf502522f1f8d5ba3
Billy Duraney <william.duraney@gmail.com>
2022-12-06 21:10:16 +0200

    add tests

--- api/src/services/wishes/wishes.test.ts
+++ api/src/services/wishes/wishes.test.ts
@@ -18,8 +18,27 @@ describe('wishes', () => {
 
   scenario('returns a single wish', async (scenario: StandardScenario) => {
     const result = await wish({ id: scenario.wish.one.id })
-
     expect(result).toEqual(scenario.wish.one)
+  })
+
+  scenario('creates a wish', async (scenario: StandardScenario) => {
+    const newWish = {
+      title: "New test wish",
+      description: "A new test wish",
+      url: "www.exmaple.com/new_test",
+    };
+    const result = await createWish({ input: newWish });
+    Object.keys(newWish).forEach(k=>expect(result[k]===newWish[k]));
+    const allWishes = await wishes()
+    expect(allWishes.length).toEqual(Object.keys(scenario.wish).length+1)
+  })
+
+  scenario('updates a wish', async (scenario: StandardScenario) => {
+    const description = "A new test wish description"
+    const result = await updateWish({ id: scenario.wish.one.id, input: {description} });
+    expect(result.description).toEqual(description)
+    expect(result.id).toEqual(scenario.wish.one.id)
+    expect(result.title).toEqual(scenario.wish.one.title)
   })
 
   scenario('deletes a wish', async (scenario: StandardScenario) => {
